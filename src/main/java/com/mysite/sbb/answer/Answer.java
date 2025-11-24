06b1d5b6b994cf10e3be93cb841c1216cc21b85f
onethousand121 <onethousand121@gmail.com>
2025-11-24 19:34:08 +0900

    2장 JSB

---
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}
---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,42 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.answer.Answer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    private LocalDateTime createDate;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    private List<Answer> answers = new ArrayList<>();
+
+    public Answer addAnswer(String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(this);
+        answer.setCreateDate(LocalDateTime.now());
+        answers.add(answer);
+
+        return answer;
+    }
+}
